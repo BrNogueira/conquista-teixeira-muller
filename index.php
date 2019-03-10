f0b2b1e57a19cd9e51ca462f6f9d287cf1387821
BrNogueira <rodrigo.nfurtado@gmail.com>
2019-03-09 23:38:49 -0300

    [conquista teixeira-muller]first layout

---
+++ index.php
@@ -0,0 +1,253 @@
+<?php include("./includes/header.php");?>
+<main>
+    <section>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="./assets/img/fullbanner.png" alt="First slide">
+                    <div class="carousel-caption">
+                        <div class="txt-destaque">
+                            <h3>Aristóteles</h3>
+                        </div>
+                        <div class="col-md-9"> 
+                            <h1>Nós somos o que fazem repetidamente.<br>A exelência, portanto, não é um ato, mas um habito</h1>
+                        </div>
+                        <a href="#" class="leia-mais">LEIA MAIS</a> 
+                    </div>
+                </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./assets/img/fullbanner.png" alt="Second slide">
+                <div class="carousel-caption">
+                        <div class="txt-fullbanner">
+                            <h3>Aristóteles</h3>
+                        </div>
+                        <div class="col-md-9"> 
+                            <h1>Nós somos o que fazem repetidamente.<br>A exelência, portanto, não é um ato, mas um habito</h1>
+                        </div>
+                        <a href="#" class="leia-mais">LEIA MAIS</a> 
+                    </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./assets/img/fullbanner.png" alt="Third slide">
+                <div class="carousel-caption">
+                        <div class="txt-fullbanner">
+                            <h3>Aristóteles</h3>
+                        </div>
+                        <div class="col-md-9"> 
+                            <h1>Nós somos o que fazem repetidamente.<br>A exelência, portanto, não é um ato, mas um habito</h1>
+                        </div>
+                        <a href="#" class="leia-mais">LEIA MAIS</a> 
+                    </div>
+            </div>
+        </div>
+
+    </div>
+    </section>
+    <section>
+        <div class="container info-container">
+            <div class="info">
+                <div class="row">	
+                    <div class="col-md-6">
+                        <div class="txt-destaque">
+                            <h3>Conheça nosso</h3>
+                            <h2>ESCRITÓRIO</h2>
+                        </div>
+                        <p>Fundado em 2006 e situado no Polo Jurídico da cidade de Porto Alegre, com localização estratégica, o Escritório Teixeira Müller Advogados Associados é um atelier jurídico de referência na advocacia consultiva e contenciosa.</p>
+                        <p>Composto por uma equipe de profissionais especializados tanto em assuntos corporativos como individuais, a sociedade advocatícia tem como principal predicado a imersão no contexto apresentado pelo cliente, sempre mediante a prévia investigação dos seus valores e premissas.</p>
+                        <a href="escritorio.php" class="leia-mais">LEIA MAIS</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="./assets/img/_MG_7811_cut.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="info-container">
+            <div class="area-pratica">
+                <div class="row">
+                    <div class="container">
+                        <div class="col-md-12">
+                            <div class="txt-destaque">
+                                <h3>Conheça nossas</h3>
+                                <h2>ÁREAS DE PRÁTICA</h2>
+                            </div>
+                        </div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="box-not row">
+                                        <img class="icon" src="./assets/img/icons/icon-work.png" alt="">
+                                        <div class="row">
+                                            <h2>Contencioso e Consultivo Trabalhista</h2>
+                                            <p>Prestação de assessoria jurídica nas mais variadas questões atintes ao Direito do Trabalho.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="box-not row">
+                                        <img class="icon" src="./assets/img/icons/icon-work2.png" alt="">
+                                        <div class="row">
+                                            <h2>Contencioso e Consultivo Cível</h2>
+                                            <p>Prestação de assessoria jurídica em questões relacionadas ao Direito Civil, Direito Administrativo...</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="box-not row">
+                                    <img class="icon" src="./assets/img/icons/icon-work3.png" alt="">
+                                        <div class="row">
+                                            <h2>Tributário Colsultivo e Contencioso</h2>
+                                            <p>Prestação de assessoria consultiva tributária e fiscal atuando na área de planejamento tributário...</p>
+                                        </div>
+                                    </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="box-not row">
+                                <img class="icon"  src="./assets/img/icons/icon-work4.png" alt="">
+                                        <div class="row">
+                                            <h2>Societário e Empresarial</h2>
+                                            <p>Assessoria jurídica em criações, incorporações e reorganizações societárias, operações de fusão...</p>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <a href="areas-de-pratica.php" class="leia-mais">LEIA MAIS</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="noticias-container">
+            <div class="noticias">
+                <div class="row">
+                    <div class="container">
+                        <div class="col-md-12">
+                            <div class="txt-destaque">
+                                <h3>Leia nossas</h3>
+                                <h2>NOTÍCIAS E ARTIGOS</h2>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="./assets/img/390x260.png" class="img-fluid">
+                                <div class="noticia">
+                                    <span class="data">
+                                        <div class="speech-bubble">
+                                            08 Mar
+                                        </div>
+                                    </span>
+                                    <div class="content-noticia">
+                                        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec malesuada lorem. Aliquam leo nibh...</p>
+                                        <a href="#" class="ler-mais">LER MAIS</a> 
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-4">
+                                <img src="./assets/img/390x260.png" class="img-fluid">
+                                <div class="noticia">
+                                    <span class="data">
+                                        <div class="speech-bubble">
+                                            08 Mar
+                                        </div>
+                                    </span>
+                                    <div class="content-noticia">
+                                        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec malesuada lorem. Aliquam leo nibh...</p>
+                                        <a href="#" class="ler-mais">LER MAIS</a> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="./assets/img/390x260.png" class="img-fluid">
+                                <div class="noticia">
+                                <span class="data">
+                                        <div class="speech-bubble">
+                                            08 Mar
+                                        </div>
+                                    </span>
+                                    <div class="content-noticia">
+                                        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec malesuada lorem. Aliquam leo nibh...</p>
+                                        <a href="#" class="ler-mais">LER MAIS</a> 
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+    <div id="carousel-brief" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="brief">
+                    <img src="./assets/img/banner-brief.jpg" alt="">
+                    <div class="carousel-caption">
+                        <div class="icon-carousel col-md-12">
+                            <img src="./assets/img/icons/icon.png" alt="">
+                        </div>
+                        <div class="col-md-12"> 
+                            <div class="brief-content">
+                                <p>“Há 12 anos sou cliente da Teixeira Muller. Quando precisamos de advogado, raramente é por bons motivos, por isso é tão importante contar com alguém que tenha empatia para compreender seus problemas e assumi-los como se fossem seus, ou seja, antes mesmo dos serviços jurídicos entrarem em cena, considero importante uma atitude de parceria autêntica: só assim é possível confiar que todos os esforços para obter êxito serão empreendidos. Dra. Vanessa, sem dúvida, é esta profissional! Uma mulher admirável e uma advogada que usa o coração e a mente afiada para brigar a seu lado pelo que é justo”.</p>
+                            </div>
+                            <div class="txt-destaque">
+                                <h3>Daniel Costa, empresário e escritor</h3> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="carousel-item">
+                <div class="brief">
+                    <img src="./assets/img/1400x400.png" alt="">
+                    <div class="carousel-caption">
+                        <div class="icon-carousel">
+                            <h3><img src="./assets/img/icons/icon.png" alt=""></h3>
+                        </div>
+                        <div class="col-md-9"> 
+                            <div class="brief-content">
+                                <h1 class="">Nós somos o que fazem repetidamente. A exelência, portanto, não é um ato, mas um habito</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="brief">
+                    <img src="./assets/img/1400x400.png" alt="">
+                    <div class="carousel-caption">
+                        <div class="icon-carousel">
+                            <h3><img src="./assets/img/icons/icon.png" alt=""></h3>
+                        </div>
+                        <div class="col-md-9"> 
+                            <div class="brief-content">
+                                <h1 class="">Nós somos o que fazem repetidamente. A exelência, portanto, não é um ato, mas um habito</h1>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div> -->
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+</main>
+<?php include("./includes/footer.php")?>
