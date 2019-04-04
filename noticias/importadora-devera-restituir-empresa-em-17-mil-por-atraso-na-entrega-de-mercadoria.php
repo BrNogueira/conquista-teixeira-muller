9f2097379103f9caf4f2c3ddf9d90b780959941b
BrNogueira <rodrigo.nfurtado@gmail.com>
2019-04-04 11:51:03 -0300

    [teixeira-muller]noticias

---
+++ noticias/importadora-devera-restituir-empresa-em-17-mil-por-atraso-na-entrega-de-mercadoria.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "../includes/head.inc.php";?>
+</head>
+<body>
+
+
+<section id="news-public">
+<div class="page">
+        <div class="container">
+            <div class="txt-destaque">
+                <h3>NOTÍCIAS E ARTIGOS</h3>
+                <h2></h2>
+            </div>
+        </div> 
+    </div>
+<div class="container">
+<div class="content">
+        <div class="content-wrapper grey">
+            <div class="news-header">
+                <h5>Importadora deverá restituir empresa em R$17 mil por atraso na entrega de mercadoria</h5>
+                        </div>
+                <div class="noticia">
+                    <img class="img-news img-fluid" src="../assets/img/noticias/importadora-devera-restituir-empresa-em-17-mil-por-atraso-na-entrega-de-mercadoria.jpg" alt="CNJ cria grupo de para debater recuperações judiciais e falências">
+                    <span class="data">
+                        <div class="speech-bubble">01 Abr</div>
+                    </span>
+                </div>
+                <div class="content-noticia ">
+                    <p class="regular-txt">Uma empresa de informática do município da Serra deverá ser indenizada por uma importadora em R$ 19.500 a títulos de danos materiais, por não receber o produto comprado dentro do prazo estabelecido. A decisão é 5ª Vara Cível da Comarca.</p>                     
+                    <p class="regular-txt">Consta no processo de nº 0008664-52.2015.8.08.0048, que para atender à licitação de uma autarquia, a empresa de informática comprou no site da importadora uma guilhotina no valor de R$ 15.800 com prazo de 90 dias para a entrega. No entanto, passado esse tempo, a mercadoria não foi entregue e foi preciso adquirir um produto semelhante em outro site, porém, no valor de R$ 17.200.</p>
+                    <p class="regular-txt">Ainda de acordo com os autos, a empresa alegou que teria enviado diversos e-mails solicitando a devolução da quantia paga, mas sem êxito. Do outro lado, a importadora argumentou que o atraso na entrega da guilhotina decorreu de caso fortuito e por motivo de força maior.</p>
+                    <p class="regular-txt">Em sua decisão, o juiz não acolheu a tese da ré, entendendo que “a burocracia oriunda da importação não se insere no contexto de caso fortuito e força maior, ao contrário, trata-se de questão inerente e de fácil previsão por parte da importadora” que trabalha diariamente com essa tarefa.</p>
+                    <p class="regular-txt">O magistrado fixou uma indenização por danos materiais no valor de R$17.624, referente à quantia atualizada desembolsada pela empresa; e mais R$1.965, necessários para cobrir a diferença de preço entre os produtos.</p>
+                    <p class="regular-txt">Quanto aos danos morais, o juiz entendeu que não houve elementos para demonstrar que os fatos causaram ofensa à parte autora, até porque a empresa teve sucesso ao prestar o serviço para o qual foi contratada.</p>
+                    <p class="regular-txt">Fonte: Tribunal de Justiça do Espírito Santo</p>
+                    <a href="../noticias.php" class="ler-mais">Voltar</a>
+                </div>
+
+        </div>
+    </div>
+</div>
+</section >
+
+<footer>
+    <?php include "../includes/footer.inc.php";?>
+</footer>
+</body>
+</html>
